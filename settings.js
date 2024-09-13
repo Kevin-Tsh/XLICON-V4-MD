cecf29c30a51dfe9642741b64241050fdbdbc611
⸢𝐌𝐑.𝐊É𝐕𝐈𝐍 𝐓𝐒𝐇⸥ <158796604+Kevin-Tsh@users.noreply.github.com>
2024-09-13 22:13:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -45,8 +45,8 @@ global.location = "Pakistan, Lahore, Sabzazar" //ur location
 
 //new
 global.botname = 'XLICON-V4-MD' //ur bot name
-global.ownernumber = ['923184070915'] //ur owner number, dont add more than one
-global.ownername = '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙' //ur owner name
+global.ownernumber = ['243975411299'] //ur owner number, dont add more than one
+global.ownername = '🌹MR KÉVIN TSH🌹' //ur owner name
 global.websitex = "https://youtube.com/@s4salmanyt"
 global.wagc = "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f"
 global.themeemoji = '⛩'
@@ -56,7 +56,7 @@ global.packname = "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉"
 global.author = "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿"
 global.creator = "923184070915@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923184070915"] // Premium User
+global.premium = ["243848825244"] // Premium User
 
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
